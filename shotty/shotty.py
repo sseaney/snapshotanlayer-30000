de72a1e69622ac8c7ed470cd3e0f2c167d0c5d94
EC2 Default User <ec2-user@ip-10-0-0-49.ec2.internal>
2020-05-26 05:50:28 +0000

    Added instance info

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,10 +1,24 @@
 import boto3
+import click
+
+
+session = boto3.Session()
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instances():
+    "List Instances"
+    for i in ec2.instances.all():
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session()
-    ec2 = session.resource('ec2')
+    list_instances()
 
-    for i in ec2.instances.all():
-        print(i)
 
 
